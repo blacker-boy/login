a90a45d6f9dc7a9855d4512d9ca0ab87d0e35f35
qiang <1391186209@qq.com>
2018-05-15 20:32:32 +0800

    除删除和修改功能外，其他功能可以实现

--- control/expend_admin.py
+++ control/expend_admin.py
@@ -8,10 +8,8 @@ import sys
 import os
 import shutil
 import imp
-
 # imp.reload(sys)
 # sys.setdefaultencoding("utf-8")
-
 sys.path.append('../')
 from config import configs
 from model import model
@@ -23,6 +21,8 @@ urls=(
     '/Register','Register',
     '/SelectTeacher','SelectTeacher',
     '/AddTeacher','AddTeacher',
+    '/DeleteTeacher','DeleteTeacher',
+    '/UpdateTeacher','UpdateTeacher',
 )
 app=web.application(urls,globals())
 
@@ -104,18 +104,18 @@ class Register:
 class SelectTeacher:
     def POST(self):
         params=web.input()
-        print(params)
+        # print(params)
         db = pymysql.connect("localhost", port=3306, user="admin", passwd="123456", db="examdb", charset='utf8')
         cursor=db.cursor()
         sql = '''select * from teacher '''
         cursor.execute(sql)
         results = cursor.fetchall()
         results=list(results)
-        print(type(results),results)
+        # print(type(results),results)
         # new_dict=dict(results)
         # print(new_dict)
-        result=json.dumps(results)
-        print(type(result),result)
+        # result=json.dumps(results)
+        # print(type(result),result)
         # return result
         cursor.close()
         db.close()
@@ -128,41 +128,44 @@ class SelectTeacher:
             data["tc_level"]=row[3]
             data["lenth"]=len(results)
             jsonResults.append(data)
-        print(results)
+        # print(results)
         # result=list(results)
         # print(type(results))
         # print("next")
+        # print(util.objtojson(jsonResults))
+        return util.objtojson(jsonResults)
+class GetTeacher:#查询单条信息
+    def POST(self):
+        web.header("Access-Control-Allow-Origin", "*")
+        # 接收参数
+        params = web.input()
+        print(params)
+        db = pymysql.connect("localhost", port=3306, user="admin", passwd="123456", db="examdb", charset='utf8')
+        cursor = db.cursor()
+        sql = "select * from teacher \
+                       where tc_id = '{}'".format(params)
+        try:
+            # 执行SQL语句
+            cursor.execute(sql)
+            # 获取所有记录列表
+            results = cursor.fetchall()
+        except:
+            # 发生错误时回滚
+            db.rollback()
+        cursor.close()
+        db.close()
+        jsonResults = []
+        for row in results:
+            data = {}
+            data["tc_id"] = row[0]
+            data["tc_name"] = row[1]
+            data["tc_password"] = row[2]
+            data["tc_level"] = row[3]
+            data["lenth"] = len(results)
+            jsonResults.append(data)
         print(util.objtojson(jsonResults))
         return util.objtojson(jsonResults)
-# class SelectTeacher:
-#     def POST(self):
-#         web.header("Access-Control-Allow-Origin", "*")
-#         # 接收参数
-#         params = web.input()
-#         # session = web.ctx.session
-#         count = model.Teacher_model.count()
-#         if params.tc_name == '' and params.tc_id == '':
-#             teacher = model.Teacher_model.query('select * from teacher ')
-#             teacher = [model.Teacher_model(**item) for item in teacher]
-#             print("teacher",teacher)
-#             page = util.Page(data=teacher, totalRow=count,
-#                              status=util.Status.__success__, message="未知")
-#             response = util.Response(status=util.Status.__success__, body=page)
-#             return util.objtojson(response)
-#         elif params.tc_name != '' and params.tc_id == '':
-#             teacher = model.Teacher_model.query('select * from teacher where tc_name ')
-#             teacher = [model.Teacher_model(**item) for item in teacher]
-#             page = util.Page(data=teacher, totalRow=teacher.__len__(),
-#                              status=util.Status.__success__, message="未知")
-#             response = util.Response(status=util.Status.__success__, body=page)
-#             return util.objtojson(response)
-#         else:
-#             teacher = model.Teacher_model.getByPK(params.tc_id)
-#             page = util.Page(data=teacher, totalRow=1),
-#                              status=util.Status.__success__, message="未知")
-#             response = util.Response(status=util.Status.__success__, body=page)
-#             print(response)
-#             return util.objtojson(response)
+
 class AddTeacher:
     def POST(self):
         params = web.input()
@@ -176,58 +179,76 @@ class AddTeacher:
         db = pymysql.connect("localhost", port=3306, user="admin", passwd="123456", db="examdb", charset='utf8')
         cursor = db.cursor()
         sql = '''select * from teacher '''
-        # try:
-        #     cursor.execute(sql)
-        #     results = cursor.fetchall()
-        #     result=list(results)
-        #     lenth=len(result)
-        #     print("len=",lenth)
-        #     for row in results:
-        #         tc_id = row
-        #         # password = row[1]
-        #         print(tc_id)
-        #         if x == tc_id:
-        #             # print('000')
-        #             return False
-        #             break
-        #     else:
-        #         print('add1')
-        #         sql = """insert into teacher(
-        #                           tc_id,
-        #                           tc_name,
-        #                           tc_password,
-        #                           tc_level)
-        #                           values('{}','{}','{}','{}')""".format(x,y,m,n)
-        #         try:
-        #             cursor.execute(sql)
-        #             db.commit()
-        #             print(51)
-        #         except:
-        #             db.rollback()
-        #     return 1
-        # except:
-        #     db.rollback()
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        print(results)
-        print(type(results))
-        print("next")
-        return results
-        result = list(results)
-        print(result)
-        print(type(result))
-        lenth = len(result)
-        print("len2=", lenth)
-        for row in results:
-            # tc_id = row
-            # password = row[1]
-            print(row)
-        #     if x == tc_id:
-        #         # print('000')
-        #         return False
-        #         break
+        try:
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            # result=list(results)
+            # lenth=len(result)
+            # print("len=",lenth)
+            for row in results:
+                tc_id = row
+                # password = row[1]
+                print(tc_id)
+                if x == tc_id:
+                    print(tc_id)
+                    return 2
+                    # break
+            else:
+                print('add1')
+                sql = """insert into teacher(
+                                  tc_id,
+                                  tc_name,
+                                  tc_password,
+                                  tc_level)
+                                  values('{}','{}','{}','{}')""".format(x,y,m,n)
+                try:
+                    cursor.execute(sql)
+                    db.commit()
+                    return 1
+                except:
+                    db.rollback()
+            # return 1
+        except:
+            db.rollback()
         cursor.close()
         db.close()
-
+class DeleteTeacher:
+    def POST(self):
+        # web.header("Access-Control-Allow-Origin", "*")
+        # 接收参数
+        params = web.input()
+        print("delete",params)
+        db = pymysql.connect("localhost", port=3306, user="admin", passwd="123456", db="examdb", charset='utf8')
+        cursor = db.cursor()
+        sql = "delete from teacher where tc_id = '{}'" .format(params)
+        try:
+            # 执行SQL语句
+            cursor.execute(sql)
+            # 提交修改
+            db.commit()
+        except:
+            # 发生错误时回滚
+            db.rollback()
+        cursor.close()
+        db.close()
+class UpdateTeacher:
+    def POST(self):
+        # web.header("Access-Control-Allow-Origin", "*")
+        # 接收参数
+        params = web.input()
+        print("delete",params)
+        db = pymysql.connect("localhost", port=3306, user="admin", passwd="123456", db="examdb", charset='utf8')
+        cursor = db.cursor()
+        sql = "update teacher where 'tc_id = '{}'".format(params)
+        try:
+            # 执行SQL语句
+            cursor.execute(sql)
+            # 提交修改
+            db.commit()
+        except:
+            # 发生错误时回滚
+            db.rollback()
+        cursor.close()
+        db.close()
 if __name__=='__main__':
     app.run()
